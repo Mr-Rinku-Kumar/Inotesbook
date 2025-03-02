e1217a4836553f2fe5618209ad5bdf4ab1b7c568
RINKU KUMAR <145314905+Mr-Rinku-Kumar@users.noreply.github.com>
2025-03-02 18:16:10 +0530

    Update Notes.jsx

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -32,7 +32,7 @@ const Notes = (props) => {
 
   const fetchUserDetails = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/getuser", {
+      const response = await fetch("https://inotesbook-backend.onrender.com/api/auth/getuser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
